2b68d76ae22f6f1892d642a0d1dfa5a0bc593bcb
Jheferson Murillo <jhefersonmurillo@gmail.com>
2022-04-24 16:11:17 -0500

    Correguir Delete

--- src/components/pacientes/Pacientes.jsx
+++ src/components/pacientes/Pacientes.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useSyncExternalStore } from "react";
 import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
 import "./Pacientes.css";
 import {
   Table,
@@ -14,8 +15,44 @@ import {
 } from "@material-ui/core";
 import { Edit, Delete } from "@material-ui/icons";
 
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  },
+  iconos: {
+    cursor: "pointer",
+  },
+  inputMaterial: {
+    width: "100%",
+  },
+}));
+
 function Pacientes() {
+  const styles = useStyles();
   const [data, setData] = useState([]);
+  const [isOpenAgree, setIsOpenAgree] = useState(false);
+  const [isOpenDelete, setIsOpenDelete] = useState(false);
+  const [isOpenEdit, setIsOpenEdit] = useState(false);
+  const [patientSelect, setPatientSelect] = useState({});
+  const [form, setForm] = useState({
+    apellido: "",
+    nombre: "",
+    email: "",
+    dni: "",
+    fechaIngreso: "",
+    calle: "",
+    numero: "",
+    localidad: "",
+    provincia: "",
+  });
 
   const getAllPatients = async () => {
     try {
@@ -27,15 +64,172 @@ function Pacientes() {
     }
   };
 
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setForm((prevState) => ({ ...prevState, [name]: value }));
+    console.log(form);
+  };
+  const createPatient = async () => {
+    const {
+      apellido,
+      nombre,
+      email,
+      dni,
+      fechaIngreso,
+      calle,
+      numero,
+      localidad,
+      provincia,
+    } = form;
+    const formToPost = {
+      apellido,
+      nombre,
+      email,
+      dni: Number(dni),
+      fechaIngreso,
+      domicilio: {
+        calle,
+        numero: Number(numero),
+        localidad,
+        provincia,
+      },
+    };
+    try {
+      const request = await axios.post(
+        "http://localhost:8080/pacientes",
+        formToPost
+      );
+      const response = request.data;
+      toogleModalAgree();
+      setData((prevState) => [...prevState, response]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleIcon = (paciente, method) => {
+    setForm(paciente);
+    method === "delete" ? toogleModalDelete : toogleModalEdit;
+  };
   useEffect(() => {
     getAllPatients();
   }, []);
 
+  const modalCreate = (
+    <div className={styles.modal}>
+      <h4>Crear Paciente</h4>
+      <TextField
+        className={styles.inputMaterial}
+        name="apellido"
+        label="Apellido"
+        onChange={handleChange}
+      />
+      <br />
+      <TextField
+        className={styles.inputMaterial}
+        name="nombre"
+        label="Nombre"
+        onChange={handleChange}
+      />
+      <br />
+      <TextField
+        className={styles.inputMaterial}
+        name="email"
+        label="email"
+        onChange={handleChange}
+      />
+      <br />
+      <TextField
+        className={styles.inputMaterial}
+        name="dni"
+        label="DNI"
+        onChange={handleChange}
+      />
+      <br />
+      <TextField
+        className={styles.inputMaterial}
+        name="fechaIngreso"
+        label="Fecha Ingreso"
+        onChange={handleChange}
+      />
+      <br />
+      <TextField
+        className={styles.inputMaterial}
+        name="calle"
+        label="Calle"
+        onChange={handleChange}
+      />
+      <br />
+      <TextField
+        className={styles.inputMaterial}
+        name="numero"
+        label="Numero Calle"
+        onChange={handleChange}
+      />
+      <br />
+      <TextField
+        className={styles.inputMaterial}
+        name="localidad"
+        label="Localidad"
+        onChange={handleChange}
+      />
+      <br />
+      <TextField
+        className={styles.inputMaterial}
+        name="provincia"
+        label="Provincia"
+        onChange={handleChange}
+      />
+      <br />
+      <div className="div">
+        <Button onClick={() => createPatient()} color="primary">
+          Insertar
+        </Button>
+        <Button onClick={() => toogleModalAgree()}>Cancelar</Button>
+      </div>
+    </div>
+  );
+
+  const modalEdit = (
+    <div className={styles.modal}>
+      <p>
+        ¿Desea eliminar el usuario <b>{form}</b>
+      </p>
+    </div>
+  );
+
+  const deletePatient = async (id) => {
+    /* const paciente = data.find((paciente) => paciente.id === id);
+    setPatientSelect({ ...paciente }); */
+    console.log("Entro");
+    console.log(id);
+    try {
+      const request = await axios.delete(
+        `http://localhost:8080/pacientes/${id}`
+      );
+      if (request.data === "OK") {
+        const pacientes = data.filter((paciente) => paciente.id !== id);
+        console.log(pacientes);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const toogleModalAgree = () => {
+    setIsOpenAgree(!isOpenAgree);
+  };
+  const toogleModalDelete = () => {
+    setIsOpenDelete(!isOpenDelete);
+  };
+  const toogleModalEdit = () => {
+    setIsOpenEdit(!isOpenEdit);
+  };
   return (
     <div>
       <h1>Pacientes</h1>
       <div>
-        <button className="button">Crear paciente</button>
+        <button className="button" onClick={() => toogleModalAgree()}>
+          Crear paciente
+        </button>
       </div>
       <Table>
         <TableHead>
@@ -66,15 +260,17 @@ function Pacientes() {
                 <TableCell>{domicilio?.localidad}</TableCell>
                 <TableCell>{domicilio?.provincia}</TableCell>
                 <TableCell>
-                  <Edit className="icons" />
-                  &nbsp;&nbsp;
-                  <Delete className="icons" />
+                  <Delete className="icon" onClick={() => deletePatient(id)} />
                 </TableCell>
               </TableRow>
             )
           )}
         </TableBody>
       </Table>
+
+      <Modal open={isOpenAgree} onClose={toogleModalAgree}>
+        {modalCreate}
+      </Modal>
     </div>
   );
 }
